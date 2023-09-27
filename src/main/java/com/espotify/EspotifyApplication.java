3bc2622f3ae2568999af0ef914ba39ecac0c68ca
William Calle Pinto <cwilliam14c@gmail.com>
2023-09-26 23:27:15 -0400

    Estructura inicial del proyecto agregado

---
+++ src/main/java/com/espotify/EspotifyApplication.java
@@ -0,0 +1,13 @@
+package com.espotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspotifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/espotify/EspotifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.espotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspotifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
